5f2d69f5e7c802ff193b821a620861e928c4e026
Fabio Leuenberger <fabioleuenberger@hotmail.com>
2022-03-08 16:51:42 +0100

    added comments

--- lab01/mC/mC.ino
+++ lab01/mC/mC.ino
@@ -2,7 +2,7 @@
 int dl, mlt, incoming;
 
 // define an array to access the LED Pins
-int led_pins[10] = {12, 27, 33, 15, 32, 14, 22, 23};
+int led_pins[8] = {12, 27, 33, 15, 32, 14, 22, 23}; 
 
 void setup() {
    
@@ -23,8 +23,8 @@ void setup() {
    pinMode(21, INPUT);      //setting GPIO that is connected to the switcher as an INPUT pin.
    int switcher = digitalRead(21);  //reading value of de GPIO to distinguish switcher position
    
-   
    //_________________End - Part A_________________
+   
 }
 
 void loop() {
@@ -39,9 +39,9 @@ void loop() {
      Serial.readBytes(((char*)&incoming),1); //in lab change this to 2 and print "incoming" below.      
      Serial.println(incoming);
      dl = incoming >> 4;
-     Serial.println(dl); 
+     Serial.println(dl); //dl is printed on the serial monitor
      mlt = incoming % (1<<4);
-     Serial.println(mlt); 
+     Serial.println(mlt); //mlt is printed on the serial monitor
    }
    //_________________End - Part B_________________
    
@@ -52,11 +52,14 @@ void loop() {
    /*
    for (j =0;j<8;j++)
    {
-     for (i=0;i<8;i++)
-       digitalWrite(led_pins[i],LOW); // belongs to second for-loop
-     digitalWrite(led_pins[j],HIGH);  // does not belong to second for-loop
+     for (i=0;i<8;i++){
+        digitalWrite(led_pins[i],LOW); 
+     }
+     
+     digitalWrite(led_pins[j],HIGH);  
      delay(dl*mlt);
-   }   // First iteration [HIGH LOW LOW LOW LOW LOW LOW LOW] second iteration [01000000], third iteration [00100000] and so on, a lighting LED moves through the array. 
+   }  
+   
    delay(dl*mlt*3);
    */
    
@@ -65,23 +68,23 @@ void loop() {
 
 
    //_________________Begin - Part D_______________
-    int arg_word_bin[8];
+    int arg_word_bin[8] = {0, 0, 0, 0, 0, 0, 0, 0}; //empty list is initialized. 
     int n = incoming;
     int u=0;
     
     for(u = 0; u <= 8; u++)
     {
-      arg_word_bin[u] = n % 2;
+      arg_word_bin[u] = n % 2;   //binary number of incoming is stored in arg_word_bin (bin order is still inverted at this point)
           n = n / 2;
     }
 
     int q=0;
     for(q=7; q>=0; q--){
-      if(arg_word_bin[q]==1){
+      if(arg_word_bin[q]==1){             //if the bit on a certain position in the arg_word_bin list is 1, the corresponding pin is set to HIGH. 
         digitalWrite(led_pins[q],HIGH);
       }
       else{
-        digitalWrite(led_pins[q],LOW);
+        digitalWrite(led_pins[q],LOW);  // all other pins are set to LOW.
       }
     }
    
