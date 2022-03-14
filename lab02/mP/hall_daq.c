9abad03814c10e10557cc13de0af937baba16e74
umbacht <thomas.umb@gmail.com>
2022-03-14 10:16:52 +0100

    Added lab02 files

---
+++ lab02/mP/hall_daq.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+
+
+// include your function definitions here
+
+
+
+int main()
+{
+    // initialize all parameters
+
+    // initialize the serial port on the port /dev/ttyUSB0, with baud rate 115200
+
+    // write to the serial port to get a value
+
+    // Let the user know if you were able to write to the port
+
+    // Read the sensor value into the buffer from the serial port
+
+    // Let the user know if you were able to read from the port
+
+    // Convert the sensor value to a voltage
+
+		// Convert the voltage value to magnetic field
+
+    // Close the serial port
+
+	return 0;
+
+}
