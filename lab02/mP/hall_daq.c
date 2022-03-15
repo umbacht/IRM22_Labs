eda28533da0214f027df44fe090eb837ec693f56
Nico Burger <nicoburger1998@gmail.com>
2022-03-15 11:29:34 +0100

    updates and make all works

--- lab02/mP/hall_daq.c
+++ lab02/mP/hall_daq.c
@@ -5,22 +5,24 @@
 
 
 // include your function definitions here
-
+#include "feather_serial.h"
+#include "hall_sensor.h"
 
 int main()
 {
     // initialize all parameters
     char buffer[4];
+    char command_read = 'r';
     int serial_reading;
     float quiescent_voltage = 1.65;
     float max_voltage = 3.3;
-    int sensitivity = 1024;
+    int sensitivity = 4095;
 
     // initialize the serial port on the port /dev/ttyUSB0, with baud rate 115200
     int serial_port = serialport_init( "/dev/ttyUSB0", 115200);    // IMPORTANT CHANGE PORT IF USING MAC!!!!
 
     // write to the serial port to get a value
-    int return_value_of_serialwrite = serialport_write(serial_port, 'r'); //sending char 'r'
+    int return_value_of_serialwrite = serialport_write(serial_port, &command_read); //sending char 'r'
 
     // Let the user know if you were able to write to the port
     if (return_value_of_serialwrite == -1){
@@ -42,7 +44,7 @@ int main()
 
     // Convert the sensor value to a voltage
     float voltage_reading_uncalibrated = serial_reading * (max_voltage/sensitivity);
-    float voltage_reading_calibrated = voltage_reading_uncalibrated - quiescent_voltage;
+    float voltage_reading_calibrated = voltage_reading_uncalibrated - quiescent_voltage;  // 0V: no magnetic field
 
 
 		// Convert the voltage value to magnetic field
