5d03e2b7a8b68cc6de1924c3e3c2d2b3d19f4492
umbacht <thomas.umb@gmail.com>
2022-03-17 08:31:05 -0600

    cleanup lab02

--- lab02/mP/hall_daq.c
+++ lab02/mP/hall_daq.c
@@ -15,7 +15,7 @@ int main()
     float quiescent_voltage = 1.65;
 
     // initialize the serial port on the port /dev/ttyUSB0, with baud rate 115200
-    int serial_port = serialport_init( "/dev/cu.SLAB_USBtoUART", 115200);    // IMPORTANT CHANGE PORT IF USING MAC!!!!
+    int serial_port = serialport_init( "/dev/cu.SLAB_USBtoUART", 115200);    // CHANGE PORT IF USING MAC
     
     // write to the serial port to get a value
     int return_value_of_serialwrite = serialport_write(serial_port, &command_read); //sending char 'r'
@@ -24,6 +24,7 @@ int main()
     if (return_value_of_serialwrite == -1){
         printf("ERROR: WAS NOT ABLE TO WRITE TO SERIAL PORT\n");
     }
+
     // Read the sensor value into the buffer from the serial port
     int serial_read_return = serialport_read(serial_port, buffer, 4, 100);
     //printf("buffer: %c", buffer);
@@ -32,9 +33,6 @@ int main()
     // Let the user know if you were able to read from the port
     if (serial_read_return == -1){
         printf("ERROR: WAS NOT ABLE TO READ FROM SERIAL PORT\n");
-    }
-    else{
-        //printf("SUCCESSFULLY READ FROM SERIAL PORT\n");
     }
 
     float field_strength = hall_sensor_get_field(serial_reading, quiescent_voltage); 
